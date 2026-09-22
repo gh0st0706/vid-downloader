696451d50840e07b263df00123f07145f4a9b03a
Ch1C0rj <166275223+Ch1C0rj@users.noreply.github.com>
2026-09-22 19:49:38 +0530

    changes to media.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from app.media import get_video_info
+
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Welcome to the Video Downloader API!"}
+
+@app.get("/info")
+def video_info(url: str):
+    return get_video_info(url)
--- app/media.py
+++ app/media.py
@@ -21,24 +21,11 @@ def download_video(url, quality):
     ydl_opts = {
         'format': f"bestvideo[height<={quality}]+bestaudio/best[height<={quality}]",
         "merge_output_format": 'mp4',
-        'outtmpl': "download\%(title)s.%(ext)s",
+        'outtmpl': "downloads/%(title)s.%(ext)s",
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-url = input("Enter the video URL: ")
 
-quality = int(input("Enter the desired quality (e.g., 720, 1080): "))
-
-download_video(url, quality)
-
-
-print("Video downloaded successfully!")
-
-info = get_video_info(url)
-
-print(f"Title: {info['title']}")
-print(f"Duration: {info['duration']} seconds")
-print(f"Number of formats: {len(info['formats'])}")
 
